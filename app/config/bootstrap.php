05ea53e22473b665ff7cb35f590ebf0748735a9d
cello-j <cello-j@klab.com>
2015-03-16 19:57:51 +0900

    add Library: router, database

--- app/config/bootstrap.php
+++ app/config/bootstrap.php
@@ -8,9 +8,14 @@ require_once APP_DIR.'app_exception.php';
 // helpers
 require_once HELPERS_DIR.'html_helper.php';
 
+//vendor
+require_once VENDOR_DIR.'SimpleDBI/src/SimpleDBI.php';
+
 // config
 require_once CONFIG_DIR.'log.php';
-require_once VENDOR_DIR.'SimpleDBI/src/SimpleDBI.php';
+require_once CONFIG_DIR.'router.php';
+require_once CONFIG_DIR.'database.php';
+
 
 spl_autoload_register(function($name) {
     $filename = Inflector::underscore($name) . '.php';
---
+++ app/config/database.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * DBI class for DietCake
+ *
+ * @license MIT License
+ * @author Tatsuya Tsuruoka <http://github.com/ttsuruoka>
+ * @link https://github.com/dietcake/dietcake-showcase
+ */
+
+class DB extends SimpleDBI
+{
+    public static function getConnectSettings($destination = null)
+    {
+        if (is_null($destination)) {
+            $dsn = DB_DSN;
+            $username = DB_USERNAME;
+            $password = DB_PASSWORD;
+            $driver_options = array(PDO::ATTR_TIMEOUT => DB_ATTR_TIMEOUT);
+        } else {
+            throw new InvalidArgumentException("Unknown database: {$destination}");
+        }
+
+        return array($dsn, $username, $password, $driver_options);
+    }
+
+    protected function onQueryEnd($sql, array $params = array())
+    {
+        $this->log($sql, $params);
+    }
+
+    protected function log($sql, array $params = array())
+    {
+        if (ENV_PRODUCTION) {
+            return;
+        }
+
+        static $num_query = 1;
+
+        preg_match('/host=([0-9A-Za-z_-]+)/', $this->dsn, $matches);
+        $host = isset($matches[1]) ? $matches[1] : '-';
+
+        Log::debug(sprintf("sql\t%f\t%d:(%s) %s; (%s)", $this->getLastExecTime(), $num_query++, $host, $sql, implode(', ', $params)), 'sql');
+    }
+}
