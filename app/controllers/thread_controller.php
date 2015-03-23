1db043ea83704feefc098ec550f732554263c2ec
cello-j <cello-j@klab.com>
2015-03-23 17:51:04 +0900

    add create thread function

--- app/controllers/thread_controller.php
+++ app/controllers/thread_controller.php
@@ -38,5 +38,31 @@ class ThreadController extends AppController
         $this->set(get_defined_vars());
         $this->render($page);
     }
-    
+
+    public function create()
+    {
+	$thread = new Thread;
+	$comment = new Comment;
+	$page = Param::get('page_next', 'create');
+
+	switch($page){
+        case 'create':
+	    break;
+	case 'create_end':
+	    $thread->title = Param::get('title');
+	    $comment->username = Param::get('username');
+	    $comment->body = Param::get('body');
+	    try{
+		$thread->create($comment);
+	    }catch (ValidationException $e){
+		    $page = 'create';
+	    }
+	    break;
+	default:
+	    throw new NotFoundException("{$page} is not found");
+	    break;
+	}
+	$this->set(get_defined_vars());
+	$this->render($page);
+    }    
 }
