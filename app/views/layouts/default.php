3c5606012c5a4307c2fbfa85edec466d0ef2c22d
cello-j <cello-j@klab.com>
2015-03-24 20:11:18 +0900

    change title and add icon

--- app/views/layouts/default.php
+++ app/views/layouts/default.php
@@ -2,8 +2,8 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <title>DietCake <?php eh($title) ?></title>
-
+    <title>Klab Forum </title>
+    <link rel="shortcut icon" type="image/x-icon" href="/bootstrap/img/logo.ico">
     <link href="/bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <style>
       body {
@@ -17,7 +17,7 @@
     <div class="navbar navbar-fixed-top">
       <div class="navbar-inner">
         <div class="container">
-          <a class="brand" href="#">DietCake Hello</a>
+          <a class="brand" href="#">KLab Forum</a>
         </div>
       </div>
     </div>
